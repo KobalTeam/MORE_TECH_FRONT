7509a71ff35b07b32c3a29108edf4d0ccc6c17bf
Andrey Radionov <andrey.radioniv.2003@gmail.com>
2022-10-07 21:21:23 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App(): JSX.Element {
+  return <p>APP</p>;
+}
+
+export default App;
---
+++ src/types/index.d.ts
@@ -0,0 +1,9 @@
+declare module "*.png" {
+  const src: any;
+  export default src;
+}
+declare module "*.jpeg";
+declare module "*.svg" {
+  const src: any;
+  export default src;
+}
